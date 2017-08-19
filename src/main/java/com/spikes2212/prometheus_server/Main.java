df280883a2f002ad4fdc54ff09fb7965b6b1a1e9
Darth Server <simon.kharmatsky@gmail.com>
2017-08-19 18:17:32 +0300

    added processArguments function to the main file

--- src/main/java/com/spikes2212/prometheus_server/Main.java
+++ src/main/java/com/spikes2212/prometheus_server/Main.java
@@ -1,6 +1,10 @@
 package com.spikes2212.prometheus_server;
 
 public class Main {
+    private static void processArguments(String[] args) {
+
+    }
+
     public static void main(String[] args) {
     }
 }
