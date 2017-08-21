1881a8a46d508f1802a3b8b549c4c10d0a277a60
Darth Server <simon.kharmatsky@gmail.com>
2017-08-21 18:26:16 +0300

    added findFirst method to TypedCollection

--- src/main/java/com/spikes2212/prometheus_server/db/TypedCollection.java
+++ src/main/java/com/spikes2212/prometheus_server/db/TypedCollection.java
@@ -1,6 +1,9 @@
 package com.spikes2212.prometheus_server.db;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.Cursor;
 import com.mongodb.DBCollection;
+import com.sun.istack.internal.Nullable;
 
 public class TypedCollection<T extends Savable> {
 
@@ -13,4 +16,17 @@ public class TypedCollection<T extends Savable> {
     public void insert(T object) {
         collection.insert(object.toDocument());
     }
+
+    @Nullable public BasicDBObject findFirst(BasicDBObject query) {
+
+        Cursor cursor = collection.find(query);
+        BasicDBObject object = (BasicDBObject) cursor.next();
+
+        for (String queryKey : query.keySet()) {
+            if (object.get(queryKey) == null) return null;
+            if (!object.get(queryKey).equals(query.get(queryKey))) return null;
+        }
+        return object;
+    }
+
 }
