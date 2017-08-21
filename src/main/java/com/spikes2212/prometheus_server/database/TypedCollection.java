7349ad85aec7db0fdbf5713b40f62c224295c9a1
Darth Server <simon.kharmatsky@gmail.com>
2017-08-21 19:27:48 +0300

    added constructor to TypedCollection

--- src/main/java/com/spikes2212/prometheus_server/database/TypedCollection.java
+++ src/main/java/com/spikes2212/prometheus_server/database/TypedCollection.java
@@ -3,6 +3,10 @@ package com.spikes2212.prometheus_server.database;
 import com.mongodb.client.MongoCollection;
 
 public class TypedCollection {
+    private MongoCollection collection;
 
+    public TypedCollection(MongoCollection collection) {
+        this.collection = collection;
+    }
 
 }
