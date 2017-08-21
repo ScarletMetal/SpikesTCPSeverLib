2c6c5754e0ad51b2ba7cce299368d5b21ebaa1dd
Darth Server <simon.kharmatsky@gmail.com>
2017-08-21 19:28:21 +0300

    made TypedCollection a generic

--- src/main/java/com/spikes2212/prometheus_server/database/TypedCollection.java
+++ src/main/java/com/spikes2212/prometheus_server/database/TypedCollection.java
@@ -2,7 +2,7 @@ package com.spikes2212.prometheus_server.database;
 
 import com.mongodb.client.MongoCollection;
 
-public class TypedCollection {
+public class TypedCollection<T extends Savable> {
     private MongoCollection collection;
 
     public TypedCollection(MongoCollection collection) {
