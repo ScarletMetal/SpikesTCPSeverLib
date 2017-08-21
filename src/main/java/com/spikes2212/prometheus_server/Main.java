b4e0bb13619c1e8b5f1bdca4cff783881604cf99
Darth Server <simon.kharmatsky@gmail.com>
2017-08-21 18:58:23 +0300

    made mongoInit load roomsDB and usersDB

--- src/main/java/com/spikes2212/prometheus_server/Main.java
+++ src/main/java/com/spikes2212/prometheus_server/Main.java
@@ -1,8 +1,12 @@
 package com.spikes2212.prometheus_server;
-
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import com.spikes2212.prometheus_server.util.LogUtil;
 
+
 public class Main {
+
+
     private static void processArguments(String[] args) {
         LogUtil.disable();
 
@@ -12,7 +16,12 @@ public class Main {
     }
 
     private static void mongoInit() {
+        MongoClient client = new MongoClient(Constants.MONGODB.HOST, Constants.MONGODB.PORT);
 
+        MongoDatabase roomsDB = client.getDatabase(Constants.MONGODB.ROOMS_DB_NAME);
+        LogUtil.data("Loading rooms db", "success");
+        MongoDatabase usersDB = client.getDatabase(Constants.MONGODB.USERS_DB_NAME);
+        LogUtil.data("loading users db", "success");
     }
 
     public static void main(String[] args) {
