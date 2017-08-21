89b60eda22b96a929552b99af001886a0ead2585
Darth Server <simon.kharmatsky@gmail.com>
2017-08-21 18:37:22 +0300

    added mongodb releated constants to Constants

--- src/main/java/com/spikes2212/prometheus_server/Constants.java
+++ src/main/java/com/spikes2212/prometheus_server/Constants.java
@@ -4,4 +4,9 @@ public class Constants {
     public static interface NETWORK {
         public static final int PORT = 2212;
     }
+
+    public static interface MONGODB {
+        public static final String HOST = "localhost";
+        public static final int PORT = 27017;
+    }
 }
