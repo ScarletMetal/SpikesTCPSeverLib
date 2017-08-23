5238617408ee206db2c3588c89d8fde129227585
Darth Server <simon.kharmatsky@gmail.com>
2017-08-23 17:41:20 +0300

    added generateIDForCollection method

--- src/main/java/com/spikes2212/prometheus_server/util/IDUtil.java
+++ src/main/java/com/spikes2212/prometheus_server/util/IDUtil.java
@@ -1,5 +1,26 @@
 package com.spikes2212.prometheus_server.util;
 
-public class IDUtil {
+import com.spikes2212.prometheus_server.Constants;
+import com.spikes2212.prometheus_server.database.TypedCollection;
+import org.bson.Document;
 
+import java.util.Random;
+
+public class IDUtil {
+    public static String generateIDForCollection(int idLength, TypedCollection collection) {
+        String id;
+        StringBuilder builder = new StringBuilder();
+
+        do {
+            for (int i = 0; i < idLength; i++) {
+                int randomID = new Random().nextInt(
+                        (Constants.ID.MAX_ID_CODE - Constants.ID.MIN_ID_CODE) + 1)
+                        + Constants.ID.MIN_ID_CODE;
+                builder.append((char) randomID);
+            }
+            id = builder.toString();
+        } while (collection.findOne(new Document("id", id)) == null);
+
+        return id;
+    }
 }
