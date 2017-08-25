bcfffee43f3fee0d9ae2e954ca178844d725ca6d
Darth Server <simon.kharmatsky@gmail.com>
2017-08-25 10:04:54 +0300

    added startListenningThread method

--- src/main/java/com/spikes2212/prometheus_server/network/Connection.java
+++ src/main/java/com/spikes2212/prometheus_server/network/Connection.java
@@ -10,7 +10,7 @@ public class Connection {
     private Socket socket;
     private DataOutputStream output;
     private BufferedReader input;
-
+    private Thread listenningThread;
     public Connection(Socket socket) throws IOException {
         this.socket = socket;
         this.input  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -25,4 +25,8 @@ public class Connection {
         return input.readLine();
     }
 
+    public void startListenningThread() {
+        listenningThread = new Thread(new ListenningRunnable(this));
+        listenningThread.start();
+    }
 }
