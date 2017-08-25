90ae132fe5a08e1d323224b50e7a54e27e2f3d33
Darth Server <simon.kharmatsky@gmail.com>
2017-08-25 10:07:47 +0300

    added constructor to ListenningRunnable

--- src/main/java/com/spikes2212/prometheus_server/network/ListenningRunnable.java
+++ src/main/java/com/spikes2212/prometheus_server/network/ListenningRunnable.java
@@ -1,4 +1,18 @@
 package com.spikes2212.prometheus_server.network;
 
-public class ListenningRunnable {
+import com.spikes2212.prometheus_server.Constants;
+import com.spikes2212.prometheus_server.util.LogUtil;
+
+import java.io.IOException;
+
+public class ListenningRunnable implements Runnable {
+
+    private Connection connection;
+    public ListenningRunnable(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void run() {
+
+    }
 }
