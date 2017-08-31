de50c2f6bcc2f0113808aa993604541da77f86fb
Darth Server <simon.kharmatsky@gmail.com>
2017-08-31 19:30:47 +0300

    added startNetwork method to accept new clients

--- src/main/java/com/spikes2212/prometheus_server/network/SocketContainer.java
+++ src/main/java/com/spikes2212/prometheus_server/network/SocketContainer.java
@@ -5,6 +5,11 @@ import com.spikes2212.prometheus_server.Constants;
 import com.spikes2212.prometheus_server.database.TypedCollection;
 import com.spikes2212.prometheus_server.network.data.Group;
 import com.spikes2212.prometheus_server.network.data.User;
+import com.spikes2212.prometheus_server.util.LogUtil;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SocketContainer {
     private TypedCollection<Group> groupsCollection;
@@ -17,6 +22,16 @@ public class SocketContainer {
     }
 
     public void startNetworking(int port) {
-
+        try {
+            ServerSocket server = new ServerSocket(port);
+            while (true) {
+                Socket socket = server.accept();
+                Connection connection = new Connection(socket);
+                connection.startListeningThread();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            LogUtil.error("IOE in startNetworking", e.toString());
+        }
     }
 }
