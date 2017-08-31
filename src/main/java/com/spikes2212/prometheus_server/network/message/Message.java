daae0fed2d0e9317f55676160b350cfb1ef92748
Darth Server <simon.kharmatsky@gmail.com>
2017-08-31 10:41:44 +0300

    added ClientMessageType interface to Message

--- src/main/java/com/spikes2212/prometheus_server/network/message/Message.java
+++ src/main/java/com/spikes2212/prometheus_server/network/message/Message.java
@@ -1,5 +1,8 @@
 package com.spikes2212.prometheus_server.network.message;
 
 public class Message {
+    public static interface ClientMessageTypeNames {
+        public static final String TEST_MESSAGE = "com.spikes2212.prometheus_client.network.message.TestMessage";
+    }
     public String stringedClass;
 }
