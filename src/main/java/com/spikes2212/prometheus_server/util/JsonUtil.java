9a76f63b396860feefcb1f4452c7f54731351f08
Darth Server <simon.kharmatsky@gmail.com>
2017-09-07 18:43:56 +0300

    rewrote jsonUtil to return HashMaps parsed from json

--- src/main/java/com/spikes2212/prometheus_server/util/JsonUtil.java
+++ src/main/java/com/spikes2212/prometheus_server/util/JsonUtil.java
@@ -1,13 +1,23 @@
 package com.spikes2212.prometheus_server.util;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class JsonUtil {
     public static String toJson(Object object) {
         return new Gson().toJson(object);
     }
 
+    /*
     public static <T> T fromJson(String json, Class<T> classOfT) {
         return new Gson().fromJson(json, classOfT);
+    }
+    */
+
+    public static Map<String, String> fromJson(String json) {
+        return new Gson().fromJson(json, new TypeToken<HashMap<String, String>>() {}.getType());
     }
 }
