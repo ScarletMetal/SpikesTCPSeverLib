715830d042f4598bc174baf1ebd87438cb07d3bf
Darth Server <simon.kharmatsky@gmail.com>
2017-09-07 18:26:59 +0300

    added id and args fields to message

--- src/main/java/com/spikes2212/prometheus_server/network/message/Message.java
+++ src/main/java/com/spikes2212/prometheus_server/network/message/Message.java
@@ -1,5 +1,9 @@
 package com.spikes2212.prometheus_server.network.message;
 
 public class Message {
+    public static interface MessageIDs{
 
+    }
+    public String id;
+    public String[] args;
 }
