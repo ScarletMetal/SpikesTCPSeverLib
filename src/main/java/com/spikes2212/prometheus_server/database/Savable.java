21eeec1b0d48d9ad2333a34953e20c884cff7bc8
Darth Server <simon.kharmatsky@gmail.com>
2017-09-08 20:38:07 +0300

    added documentation to Savable

--- src/main/java/com/spikes2212/prometheus_server/database/Savable.java
+++ src/main/java/com/spikes2212/prometheus_server/database/Savable.java
@@ -2,11 +2,22 @@ package com.spikes2212.prometheus_server.database;
 
 import org.bson.Document;
 
+/**
+ * An interface that is design to make an object to be parsed from and to {@link Document}
+ */
 public interface Savable {
 
     public String id = "";
-    
+
+    /**
+     * A method that returns the data contained in the object parsed to {@link Document}
+     * @return {@link Document} object that contains the data of the object
+     */
     public Document toDocument();
 
+    /**
+     * A method that receives an {@link Document} object and sets the data of the object to the data in the {@link Document}
+     * @param object {@link Document} instance that contains data
+     */
     public void fromDocument(Document object);
 }
