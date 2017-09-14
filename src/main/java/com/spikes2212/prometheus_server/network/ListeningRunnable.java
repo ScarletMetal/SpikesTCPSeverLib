353e4623b4444b6a10eacb15b22f4fa065635c4d
Darth Server <simon.kharmatsky@gmail.com>
2017-09-14 18:43:04 +0300

    added javadoc for ListeningRunnable.java

--- src/main/java/com/spikes2212/prometheus_server/network/ListeningRunnable.java
+++ src/main/java/com/spikes2212/prometheus_server/network/ListeningRunnable.java
@@ -10,11 +10,29 @@ import com.spikes2212.prometheus_server.util.LogUtil;
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ * A {@link Runnable} that is passed to {@link Connection#listeningThread}
+ * This Class is responsible for receiving messages that are sent through the internet and process them.
+ *
+ * @see java.lang.Runnable
+ * @see Connection
+ * @see TypedCollection
+ * @author Simon "C" Kharmatsky
+ */
 public class ListeningRunnable implements Runnable {
 
     private Connection connection;
     private TypedCollection<Group> groupsCollection;
     private TypedCollection<User> usersCollection;
+
+    /**
+     * A constructor that constructs {@link ListeningRunnable} using
+     * {@link TypedCollection<User>}, {@link TypedCollection<Group>} and {@link Connection} instances
+     *
+     * @param connection {@link Connection} instance that is used by {@link ListeningRunnable}
+     * @param groupsCollection {@link TypedCollection<User>} instance that is used by {@link ListeningRunnable}
+     * @param usersCollection {@link TypedCollection<Group>} instance that is used by {@link ListeningRunnable}
+     */
     public ListeningRunnable(Connection connection, TypedCollection<Group> groupsCollection,
                              TypedCollection<User> usersCollection) {
         this.connection = connection;
@@ -22,6 +40,11 @@ public class ListeningRunnable implements Runnable {
         this.usersCollection = usersCollection;
     }
 
+    /**
+     * Run Method, overridden from {@link Runnable#run()}
+     * This Method start listening loop for the {@link ListeningRunnable#connection}
+     * and process messages received by {@link Connection#input}
+     */
     public void run() {
         try {
             String data;
@@ -34,6 +57,11 @@ public class ListeningRunnable implements Runnable {
             ioe.printStackTrace();
         }
     }
+
+    /**
+     * A method that processes message from {@link Map} received by {@link ListeningRunnable#connection}
+     * @param messageMap the {@link Map} instance received from {@link ListeningRunnable#connection}
+     */
     private void processMessageMap(Map<String, String> messageMap) {
         Message msg = null;
         // processing message from messageMap
